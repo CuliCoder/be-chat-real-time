f56baf5a1014a70af2c28355e87d1d3ad58d02ed
trungnc113 <nguyencongtrung13@gmail.com>
2024-08-01 13:50:10 +0700

    push 12

--- src/middleware/csrf_token.js
+++ src/middleware/csrf_token.js
@@ -14,7 +14,8 @@ export const csrfMiddleware = (req, res, next) => {
   if (req.method === "GET") {
     const csrfToken = crypto.randomBytes(32).toString("hex");
     res.cookie("_csrf", csrfToken, {
-      sameSite: "Lax", // Hoặc 'Lax', tùy thuộc vào yêu cầu của bạn
+      sameSite: "None",
+      secure: true, // Hoặc 'Lax', tùy thuộc vào yêu cầu của bạn
     });
     res.locals.csrfToken = csrfToken; // Để có thể gửi token trong response nếu cần
   }
