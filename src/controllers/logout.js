3a13aee212a4a658b5f57e15f4a1b9107606740d
trungnc113 <nguyencongtrung13@gmail.com>
2024-08-30 23:57:55 +0700

    30/8 1

--- src/controllers/logout.js
+++ src/controllers/logout.js
@@ -1,7 +1,11 @@
 export const logout = async (req, res) => {
   try {
     Object.keys(req.cookies).forEach((key) => {
-      res.clearCookie(key);
+      res.clearCookie(key, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "None",
+      });
     });
     return res.status(200).json({
       error: 0,
